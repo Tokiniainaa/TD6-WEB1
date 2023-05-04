68aa9f772ae1d2e407c9bcf24a2fea21228c06a9
tokiniaina <hei.tokiniaina@gmail.com>
2023-05-04 22:55:59 +0300

    modification des design

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const formulaire = document.getElementById("formulaire");
-const button =document.getElementById("button");
-const nom =document.getElementById("nom");
-const prenom =document.getElementById("prenom");
-const cancel =document.getElementById("cancel");
-button.onclick=function(){
-    formulaire.style.opacity=("1");
-    button.style.opacity=("0.5");
-    
+const button = document.getElementById("button");
+const nom = document.getElementById("nom");
+const prenom = document.getElementById("prenom");
+const cancel = document.getElementById("cancel");
+const email = document.getElementById("Email");
+button.onclick = function () {
+    formulaire.style.opacity = ("1");
+    button.style.opacity = ("0.1");
+
 
 }
 
-cancel.onclick=function(){
-    let valeur2=prenom.value
-let valeur =nom.value;
-formulaire.style.opacity=("0.5");
-button.style.opacity=("1")
-setTimeout (()=>{
-    alert(valeur +" "+valeur2)
-},1);
+cancel.onclick = function () {
+    let valeur2 = prenom.value
+    let valeur = nom.value;
+    let valeur3 = email.value;
+    if ((valeur == '') || (valeur2 == '') || (valeur3 == '')) {
+        alert("Fill it Please")
+    }
+    else {
+        formulaire.style.opacity = ("0.5");
+        button.style.opacity = ("1")
+        setTimeout(() => {
+            alert("Welcome " + valeur + " " + valeur2)
+        }, 1);
+    }
+
+
 }
 
 
